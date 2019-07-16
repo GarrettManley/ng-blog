44b5707788932176b2e6fc78585ef09febcc3792
Garrett Manley <GarrettManley@gmail.com>
2019-07-16 16:01:51 -0500

    Added additional methods to the ShellHelper. -  Added cross platform handlers -  Updated documentation

--- src/helpers/shell.ts
+++ src/helpers/shell.ts
@@ -1,9 +1,51 @@
 import { execSync } from 'child_process';
 
 export class ShellHelper {
+    platform: NodeJS.Platform = process.platform;
+
     constructor() {}
 
+    /**
+     * ## Run Shell Command
+     * executes a command in the local node environment
+     * @param cmd The command to be run by the node child process
+     */
     public run(cmd: string): string {
         return execSync(cmd, { encoding: 'utf-8' });
+    }
+
+    /**
+     * ## Remove Directory
+     * Removes a directory from a file system.
+     * @param dirPath The path of the directory you want to remove
+     */
+    public rmDir(dirPath: string) {
+        const cmdWindows: string = `rmdir /s ${dirPath}`;
+        const cmdUnix: string = `rm -rf ${dirPath}`;
+
+        this.runXP(cmdWindows, cmdUnix);
+    }
+
+    /**
+     * ## Run Cross Platform
+     * Checks for the OS platform of the execution environment and runs
+     * the appropriate command
+     * @param cmdWindows The command on a windows environment
+     * @param cmdUnix The command on a unix environment
+     */
+    private runXP(cmdWindows: string, cmdUnix: string): string {
+        if (this.isWindows()) {
+            return this.run(cmdWindows);
+        } else {
+            return this.run(cmdUnix);
+        }
+    }
+
+    /**
+     * Checks to see if the Node environment is running on a Windows machine
+     */
+    private isWindows(): boolean {
+        const windows = 'win32';
+        return this.platform === windows;
     }
 }

739e1e62423616bbaecac05bd5e86fe502a7b6ad
Garrett Manley <garrett.manley@perficient.com>
2019-07-29 13:42:07 -0500

    cleaned up sample app bootstrap

--- ng-blog/projects/sample-app/src/app/app.component.ts
+++ ng-blog/projects/sample-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'blog-sample-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sample-app';

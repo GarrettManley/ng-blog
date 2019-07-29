d79a982300b2e83c68fd43bf2bdfd377152f0a6f
Garrett Manley <garrett.manley@perficient.com>
2019-07-29 13:26:26 -0500

    created blog component library

---
+++ ng-blog/projects/ng-blog/src/lib/ng-blog.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgBlogComponent } from './ng-blog.component';
+
+
+
+@NgModule({
+  declarations: [NgBlogComponent],
+  imports: [
+  ],
+  exports: [NgBlogComponent]
+})
+export class NgBlogModule { }

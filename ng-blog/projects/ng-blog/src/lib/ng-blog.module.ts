0fde3d18c72bd44e9b20527f57849a0f39f96f15
Garrett Manley <garrett.manley@perficient.com>
2019-07-29 16:01:42 -0500

    generated blog component and added to sample app

--- ng-blog/projects/ng-blog/src/lib/ng-blog.module.ts
+++ ng-blog/projects/ng-blog/src/lib/ng-blog.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, BlogComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, BlogComponent],
 })
 export class NgBlogModule {}

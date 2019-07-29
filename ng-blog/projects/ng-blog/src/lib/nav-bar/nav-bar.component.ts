1674cb0155b2c02237bbd9f79610ede86eca6138
Garrett Manley <garrett.manley@perficient.com>
2019-07-29 14:17:33 -0500

    Created basic navbar setup

--- ng-blog/projects/ng-blog/src/lib/nav-bar/nav-bar.component.ts
+++ ng-blog/projects/ng-blog/src/lib/nav-bar/nav-bar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ng-blog-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
+  @Input()
+  title = 'ng-blog';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

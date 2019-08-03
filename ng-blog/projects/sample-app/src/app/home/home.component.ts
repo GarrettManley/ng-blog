54eb7ad62050d51a2b1c52a21af947ae087df1ba
Your NameGarrett <garrettmanley@gmail.com>
2019-08-03 11:39:07 -0500

    Cleaned unused declearations

--- ng-blog/projects/sample-app/src/app/home/home.component.ts
+++ ng-blog/projects/sample-app/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { IBlog } from 'projects/ng-blog/src/lib/blog/blog.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'blog-sample-home',

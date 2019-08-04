2402732d839ab11d51a4791d3db8002bb5b39953
Your NameGarrett <garrettmanley@gmail.com>
2019-08-04 00:02:42 -0500

    seperated single run headless tests vs dev tests

--- ng-blog/projects/ng-blog/karma.conf.js
+++ ng-blog/projects/ng-blog/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome'],
 
     // you can define custom flags
     customLaunchers: {

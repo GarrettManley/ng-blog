f4cdc61f7a465eecdafc8f9985dc9e97a351ce31
Your NameGarrett <garrettmanley@gmail.com>
2019-08-03 23:39:14 -0500

    Setup tests to run headless with no sandbox in chrome

--- ng-blog/projects/ng-blog/karma.conf.js
+++ ng-blog/projects/ng-blog/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadlessNoSandbox'],
 
     // you can define custom flags
     customLaunchers: {

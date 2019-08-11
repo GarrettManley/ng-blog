ffd8a9ed99736e9995d5069bcaed600d4efccb8d
Your NameGarrett <garrettmanley@gmail.com>
2019-08-11 18:55:45 -0500

    added fetch git remote change

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -11,6 +11,7 @@ sudo chmod 600 .travis/github_deploy_key
 eval $(ssh-agent -s)
 ssh-agent $(ssh-add .travis/github_deploy_key)
 
+git remote set-url origin git@github.com:GarrettManley/ng-blog.git
 git remote set-url --push origin git@github.com:GarrettManley/ng-blog.git
 
 git remote -v

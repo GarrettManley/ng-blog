b879a62fdef463482185b38cefb8c4c403d38088
Your NameGarrett <garrettmanley@gmail.com>
2019-08-11 13:37:10 -0500

    added caching of node modules temporarily disabled pushing of new build number

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ npm run build
 npm version patch
 
 # Push the version patch to the head of develop so that this changes is reflected
-VERSION_HASH=$(git rev-parse HEAD)
-git push origin $VERSION_HASH:develop
+# VERSION_HASH=$(git rev-parse HEAD)
+# git push origin $VERSION_HASH:develop

f16e6a3b4d57966b6401229e17ab3e0a3ba3a168
Garrett Manley <garrett.manley@perficient.com>
2019-08-15 12:10:49 -0500

    removed tests from pre-install to pre-build

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -10,5 +10,3 @@ sudo chmod 600 .travis/github_deploy_key
 
 git remote set-url origin git@github.com:GarrettManley/ng-blog.git
 git remote set-url --push origin git@github.com:GarrettManley/ng-blog.git
-
-npm test

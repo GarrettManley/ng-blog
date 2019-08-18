d0cbeae79493fade2a8cc49625a981b7f0c86f5c
Garrett Manley <garrett.manley@perficient.com>
2019-08-18 13:06:20 -0500

    uncommneted code now that fixed .

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env sh
-# TRAVIS_PULL_REQUEST=$0
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo 'RTESDF  TEST TESRTT  T TEST.'
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+	npm version patch -m "[skip ci] %s"
+	# add gitup deploy key to the SSH agent
+	eval $(ssh-agent -s)
+	ssh-agent $(ssh-add .travis/github_deploy_key)
 
-	# npm version patch -m "[skip ci] %s"
-	# # add gitup deploy key to the SSH agent
-	# eval $(ssh-agent -s)
-	# ssh-agent $(ssh-add .travis/github_deploy_key)
-
-	# # Push the version patch to the head of develop so that this changes is reflected
-	# VERSION_HASH=$(git rev-parse HEAD)
-	# git push origin ${VERSION_HASH}:develop --follow-tags
+	# Push the version patch to the head of develop so that this changes is reflected
+	VERSION_HASH=$(git rev-parse HEAD)
+	git push origin ${VERSION_HASH}:develop --follow-tags
 else
 	echo 'Version Number not updated. This is a PR.'
 fi

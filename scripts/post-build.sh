02158d6440cb2d388fc55a89995294acfd72d1bd
Garrett Manley <garrett.manley@perficient.com>
2019-08-18 12:07:38 -0500

    updated potential issue with running script check for version

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
-# Do not update build number if this is a pull request
-if ["$TRAVIS_PULL_REQUEST" = "false"]; then
-	npm version patch -m "[skip ci] %s"
-	# add gitup deploy key to the SSH agent
-	eval $(ssh-agent -s)
-	ssh-agent $(ssh-add .travis/github_deploy_key)
+npm version patch -m "[skip ci] %s"
+# add gitup deploy key to the SSH agent
+eval $(ssh-agent -s)
+ssh-agent $(ssh-add .travis/github_deploy_key)
 
-	# Push the version patch to the head of develop so that this changes is reflected
-	VERSION_HASH=$(git rev-parse HEAD)
-	git push origin ${VERSION_HASH}:develop --follow-tags
-fi
+# Push the version patch to the head of develop so that this changes is reflected
+VERSION_HASH=$(git rev-parse HEAD)
+git push origin ${VERSION_HASH}:develop --follow-tags

a603a3519c3e85320cf60c961332ea7c48f66e69
Garrett Manley <garrett.manley@perficient.com>
2019-08-18 12:30:51 -0500

    IDK man

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -8,3 +8,5 @@ ssh-agent $(ssh-add .travis/github_deploy_key)
 # Push the version patch to the head of develop so that this changes is reflected
 VERSION_HASH=$(git rev-parse HEAD)
 git push origin ${VERSION_HASH}:develop --follow-tags
+
+exit 0
